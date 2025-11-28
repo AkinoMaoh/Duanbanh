f1625be38a057cb8f6527276e1104cd7e25bf3f3
AkinoMaoh <ankinoto20@gmail.com>
2025-11-28 20:03:31 +0700

    thaydoigiaodien

--- Model/pdo.php
+++ Model/pdo.php
@@ -1,7 +1,7 @@
 <?php
 /* function kết nối database */
 function pdo_get_connection(){
-    $dburl = "mysql:host=localhost;dbname=nhansu;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=duanbanhngot;charset=utf8";
     $username = 'root';
     $password = '';
 
