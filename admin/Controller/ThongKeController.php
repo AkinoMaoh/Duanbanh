1aef6ac40cc6414f679b9aba7bb727b8e9a78e06
AkinoMaoh <ankinoto20@gmail.com>
2025-12-11 12:38:59 +0700

    chỉnh  sửa

--- admin/Controller/ThongKeController.php
+++ admin/Controller/ThongKeController.php
@@ -1,18 +1,16 @@
 <?php
 
-use function PHPSTORM_META\map;
-
 include_once("Model/ThongKe.php");
 class ThongKeController
 {
-    private $sanPham;
+
     private $danhMuc;
     private $thongKe;
     
 
     public function __construct()
     {
-        $this->sanPham = new SanPham();
+     
         $this->danhMuc = new DanhMuc();
         $this->thongKe = new ThongKe();
     }
@@ -20,18 +18,19 @@ class ThongKeController
     // Phương thức list
     public function index()
     {
-        $allSanPham = $this->thongKe->getAll();
+     
          $soLuongDaBan = $this->thongKe->soLuongBan();
-    $tongDaBan = isset($soLuongDaBan['soLuongDaBan']) ? $soLuongDaBan['soLuongDaBan'] : 0;
+        $tongDaBan = isset($soLuongDaBan['soLuongDaBan']) ? $soLuongDaBan['soLuongDaBan'] : 0;
+        $tongDoanhThu = $this->thongKe->doanhThu();
+        $tongDon = $this->thongKe->tongDonHang();
          $hienThiTK = $this->thongKe->getTK();
          $productsNew = $this->thongKe->getAllNew();
           $productsBanChay = $this->thongKe->getAllBanChay();
           foreach($productsNew as $key => $item) {
             $productsNew[$key]['tendanhmuc'] = $this->danhMuc->getOne($item['iddm'])['name'];
         }
-        foreach($allSanPham as $key => $item) {
-            $allSanPham[$key]['tendanhmuc'] = $this->danhMuc->getOne($item['iddm'])['name'];
-        }
+       
+        
         include_once("./views/thongke/list.php");
     }
     public function top10SpNew()
